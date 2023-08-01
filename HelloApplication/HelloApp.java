17807ebf6f735cfe0d31432761b88cbe818745f6
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-08-01 11:35:56 +0900

    Update HelloApp.java

--- HelloApplication/HelloApp.java
+++ HelloApplication/HelloApp.java
@@ -1,6 +1,11 @@
 package HelloApplication;
+//==========================================
+// Simple Hello Application in Java
+//==========================================
+import java.lang.*;
 
 public class HelloApp {
+    
     public static void main(String[] args) {
         System.out.println("Hello");
     }
